c372620213dde9be092ce46d1a850d9e9732c41f
Jake Smith <jakehockey10@gmail.com>
2016-12-26 12:40:45 -0700

    Adding Course Buttons

--- src/assets/main.js
+++ src/assets/main.js
@@ -7,7 +7,7 @@ $(function() {
     success: function(response) {
       // handle response
       for (var course of response.courses.completed) {
-        $('#badges').append('<div class="course"><h3>' + course.title + '</h3><img src="' + course.badge + '"></div>');
+        $('#badges').append('<div class="course"><h3>' + course.title + '</h3><img src="' + course.badge + '"><a href="' + course.url + '" target="_blank" class="btn btn-primary">See Course</a></div>');
       }
     }
   });
