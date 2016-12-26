017dcfa4fb4255a31eb3173079d78ec7620c1e7e
Jake Smith <jakehockey10@gmail.com>
2016-12-26 12:32:26 -0700

    Make an Ajax Call

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,12 @@
 $(function() {
 
   // your code will go here
+  $.ajax({
+    url: 'https://www.codeschool.com/users/jakehockey10.json',
+    dataType: 'jsonp',
+    success: function(response) {
+      // handle response
+    }
+  });
 
 });
